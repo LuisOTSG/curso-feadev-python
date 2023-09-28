55e8acba19f95b7949540a0418bbcfc4f7c5f504
Luis Gomes <luis12osg@gmail.com>
2023-09-28 20:42:35 -0300

    Semana 1 arquivos no jupyter notebook

---
+++ Semana 1/teste.py
@@ -0,0 +1,16 @@
+# %%
+print("Hello World!")
+
+# %%
+x = 5
+y = x ** 2
+print(x + y)
+
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+x = [0, 1, 2, 3, 4, 5]
+y = [ 0, 1, 4, 9, 16]
+plt.plot(x, y)
+# %%
